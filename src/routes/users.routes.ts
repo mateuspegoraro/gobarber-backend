cc3c72658094b59c79abcd2e59d9d3b65f295685
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-07-04 19:31:05 -0300

    Tratamento global de erros utilizando um middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,23 +11,19 @@ const usersRouter = Router();
 const upload = multer(uploadConfigs);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUserService = new CreateUserService();
+  const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUserService.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -35,19 +31,15 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const { filename } = request.file;
+    const { filename } = request.file;
 
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFileName: filename,
-      });
-      delete user.password;
-      return response.json(user);
-    } catch (err) {
-      return response.status(err.statusCode).json({ error: err.message });
-    }
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: filename,
+    });
+    delete user.password;
+    return response.json(user);
   },
 );
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import routes from './routes';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
+import routes from './routes';
 import 'reflect-metadata';
 import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -12,6 +14,21 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log(`Server Started at ${Date.now()}`);
