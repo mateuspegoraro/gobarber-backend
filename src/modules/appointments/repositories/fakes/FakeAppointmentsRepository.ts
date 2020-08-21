a1d5e51e9e4310a0e24716948ea9ca345a1bd7ac
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-08-21 10:45:50 -0300

    Testes de agendamento

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
@@ -7,8 +8,8 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
@@ -20,11 +21,7 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
 
-    // Object.assign(appointment, { id: uuid(), date, provider_id });
-
-    appointment.id = uuid();
-    appointment.date = date;
-    appointment.provider_id = provider_id;
+    Object.assign(appointment, { id: uuid(), date, provider_id });
 
     this.appointments.push(appointment);
 
--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -16,7 +17,23 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
   });
 
-  // it('should not be able to create two appointments on the same datetime', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments on the same datetime', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123456',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
