2c5949af8f999989cced12633d1dedc1415a307c
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-08-22 10:53:46 -0300

    Testando criação de usuário

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUserService = new CreateUserService(fakeUserRepository);
+
+    const user = await createUserService.execute({
+      name: 'Test',
+      email: 'test@test.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create two users with the same email', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUserService = new CreateUserService(fakeUserRepository);
+
+    await createUserService.execute({
+      name: 'Test2',
+      email: 'test2@test.com',
+      password: '123456',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'Test2',
+        email: 'test2@test.com',
+        password: '1234567',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
