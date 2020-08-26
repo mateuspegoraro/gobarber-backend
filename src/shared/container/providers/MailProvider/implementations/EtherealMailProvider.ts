63d003184c95b2aa228cb6067c6afec823bd15a8
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-08-26 15:05:52 -0300

    Template de emails

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,39 +1,55 @@
 import nodemailer, { Transporter } from 'nodemailer';
-import IMailprovider from '../models/IMailProvider';
 
+import { injectable, inject } from 'tsyringe';
+
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import IMailprovider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+
+@injectable()
 export default class EtherealMailProvider implements IMailprovider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
     // Only needed if you don't have a real mail account for testing
     nodemailer.createTestAccount().then(account => {
       // create reusable transporter object using the default SMTP transport
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
         port: account.smtp.port,
-        secure: false, // true for 465, false for other ports
+        secure: false,
         auth: {
-          user: account.user, // generated ethereal user
-          pass: account.pass, // generated ethereal password
+          user: account.user,
+          pass: account.pass,
         },
       });
       this.client = transporter;
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: '"Equipe GoBarber" <equipe@gobarber.com.br>', // sender address
-      to, // list of receivers
-      subject: 'Recuperação de senha', // Subject line
-      text: body, // plain text body
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   }
 }
