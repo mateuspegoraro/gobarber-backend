4b1740a81fcf1e5aa91959cb77a0cf7642172068
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-08-29 11:32:49 -0300

    Excluindo horários antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -24,13 +24,17 @@ describe('ListProviderDayAvailability', () => {
     });
 
     await fakeAppointmentsRepository.create({
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2020, 4, 20, 14, 0, 0),
       provider_id: provider.id,
     });
 
     await fakeAppointmentsRepository.create({
-      date: new Date(2020, 4, 20, 10, 0, 0),
+      date: new Date(2020, 4, 20, 15, 0, 0),
       provider_id: provider.id,
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -48,14 +52,26 @@ describe('ListProviderDayAvailability', () => {
         },
         {
           hour: 9,
-          available: true,
+          available: false,
         },
         {
           hour: 10,
           available: false,
         },
         {
-          hour: 11,
+          hour: 13,
+          available: true,
+        },
+        {
+          hour: 14,
+          available: false,
+        },
+        {
+          hour: 15,
+          available: false,
+        },
+        {
+          hour: 16,
           available: true,
         },
       ]),
