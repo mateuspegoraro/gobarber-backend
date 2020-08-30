8ec42f42e70e16d584163011adfafacc811dd0a7
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-08-30 12:15:37 -0300

    Rotas e controllers

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -4,12 +4,28 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 
 import ProvidersController from '../controllers/ProvidersController';
 
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+
 const providersRouter = Router();
 
 providersRouter.use(ensureAuthenticated);
 
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
