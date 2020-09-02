b76edfde4bc09a30ab05c81980c19cd4eda92ccf
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-09-02 07:47:16 -0300

    Utilizando Class Transformer

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -4,6 +4,8 @@ import { container } from 'tsyringe';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
 
+import { classToClass } from 'class-transformer';
+
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
@@ -11,9 +13,7 @@ export default class ProfileController {
 
     const user = await showProfileService.execute(user_id);
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -32,6 +32,6 @@ export default class ProfileController {
 
     delete user.password;
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
