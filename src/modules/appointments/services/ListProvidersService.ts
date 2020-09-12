47e2df2d8b6303cefb2a631dc6145c2cce8bdc84
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-09-12 12:06:54 -0300

    Cache lista de providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,7 +1,7 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import { injectable, inject } from 'tsyringe';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
-import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 interface IRequest {
@@ -13,14 +13,18 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
-    if (!users || users.length < 1) {
-      throw new AppError('No users found');
+    const key = `providers-list:${user_id}`;
+    let users = await this.cacheProvider.recover<User[]>(key);
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+      await this.cacheProvider.save(key, users);
     }
 
     return users;
