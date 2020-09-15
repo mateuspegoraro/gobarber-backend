d9897f6eb0886ffa6fb5330ff80f0dc084411787
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-09-15 07:43:04 -0300

    Express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import { errors } from 'celebrate';
 import 'reflect-metadata';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/RateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -15,16 +16,12 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
-
 app.use(express.json());
-
 app.use('/files', express.static(uploadConfig.uploadsFolder));
-
 app.use(routes);
-
 app.use(errors());
-
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
