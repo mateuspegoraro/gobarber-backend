a37b2fd9569bf89cc3f425c92f347fff21d88a1d
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-09-22 19:21:57 -0300

    Criando Header

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -34,7 +34,7 @@ class User {
   @UpdateDateColumn()
   updated_at: Date;
 
-  @Expose({ name: 'avatar' })
+  @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
     if (!this.avatar) {
       return null;
