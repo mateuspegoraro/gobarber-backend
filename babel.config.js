1a2edb28c53add1908cb5ea97371bd01fff8e999
Mateus <mateus.pegoraro@perceptvision.com.br>
2021-02-28 11:42:49 -0300

    Gerando build do projeto

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    // Converte para a versão do node instalada (é bom que seja LTS)
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@modules': './src/modules',
+        '@config': './src/config',
+        '@shared': './src/shared'
+      }
+    }],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }]
+  ],
+}
